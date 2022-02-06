1dd4bffa8abf91a0eda7e4dfb5a96b1d4d067554
WYS <silence14@hanmail.net>
2022-02-06 16:47:05 +0900

    Update ping

--- migrations/sqls/20220206071322-wallet-up.sql
+++ migrations/sqls/20220206071322-wallet-up.sql
@@ -1,1 +1,10 @@
 /* Replace with your SQL commands */
+
+CREATE TABLE `wallets` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `wallet_address` varchar(1000) NOT NULL DEFAULT '',
+    `balance` int(11) unsigned NOT NULL DEFAULT 0,
+    `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_updated` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
